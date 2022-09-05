c4e2b10b58ddc0fcaf23b04d5f40bfcea178b8da
Yamingue <yamking01@gmail.com>
2022-09-05 12:00:56 +0100

    ajout de code pdf

--- Screens/CodeRoutier.js
+++ Screens/CodeRoutier.js
@@ -1,46 +1,86 @@
-import React from 'react'
-import { View, Text, Image, ScrollView } from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import Pdf from 'react-native-pdf';
 import code_data from '../Data/Code';
 
-const DataView = ({ item }) =>{
-    return(
-        <View>
-            <Text
-                style={{
-                    fontWeight:'bold',
-                    fontSize:18,
-                    textAlign:'center',
-                    margin:10
-                }}
-            >{item.title}</Text>
-            <Image
-            source={item.image}
-            style={{
-                height:180,
-                resizeMode:'contain',
-                width:'100%'
-            }}
-            />
-            <Text>
-               {item.text}
-            </Text>
-        </View>
-    );
-}
+const DataView = ({item}) => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 18,
+          textAlign: 'center',
+          margin: 10,
+        }}>
+        {item.title}
+      </Text>
+      <Image
+        source={item.image}
+        style={{
+          height: 180,
+          resizeMode: 'contain',
+          width: '100%',
+        }}
+      />
+      <Text>{item.text}</Text>
+    </View>
+  );
+};
 
 export default class CodeRoutier extends React.Component {
-    render(){
-        return (
-         <ScrollView
-         style={{
-             padding:10,
-             flex:1
-         }}
-         >
-             {
-                 code_data.map((v,k)=><DataView item={v} key={k}/>)
-             }
-         </ScrollView>
-        );
-    }
+  render() {
+    let source = {uri: 'bundle-assets://code_route.pdf'};
+    return (
+        <View style={styles.container}>
+            <Pdf
+                source={source}
+                style={styles.pdf}/>
+        </View>)
+    return (
+      <ScrollView
+        style={{
+          padding: 10,
+          flex: 1,
+        }}>
+        {code_data.map((v, k) => (
+          <DataView item={v} key={k} />
+        ))}
+        <Pdf
+        enableRTL
+          source={source}
+          onLoadComplete={(numberOfPages, filePath) => {
+            console.log(`Number of pages: ${numberOfPages}`);
+          }}
+          onPageChanged={(page, numberOfPages) => {
+            console.log(`Current page: ${page}`);
+          }}
+          onError={error => {
+            console.log(error);
+          }}
+          onPressLink={uri => {
+            console.log(`Link pressed: ${uri}`);
+          }}
+          style={styles.pdf}
+        />
+      </ScrollView>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
